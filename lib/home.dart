209b7e7c3d639c8344f40bcd0d3cadf57d00b1a6
David Speers <s1553593@sms.ed.ac.uk>
2019-06-29 21:37:26 +0100

    Changed FutureBuilder to Stateful widget with addPostFrameCallback

--- lib/home.dart
+++ lib/home.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'models/post.dart';
 import 'components/posts_list_view.dart';
 import 'utils/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
 
   HomePage({Key key, this.title}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Widget body = Center(child: CircularProgressIndicator());
+
+  void initState() {
+    super.initState();
+    /// This function is executed one time, after the initial layout is built.
+    /// This approach was required instead of a FutureBuilder because using a
+    /// FutureBuilder was calling the fetchPosts method multiple times upon startup.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      List<Post> posts = await fetchPosts(BELFAST_WOE_ID);
+      setState(() {
+        body = PostsListView(posts: posts);
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: fetchPosts(BELFAST_WOE_ID),
-        builder: (context, snapshot) {
-          return snapshot.hasData
-              ? PostsListView(posts: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
-      ),
+      body: body
     );
   }
 }
